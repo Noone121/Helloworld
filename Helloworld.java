34bebd10e8d99ede02b6d1b6745a3dbf2ea2db93
phamt <phamtat0510@gmail.com>
2024-11-06 23:43:55 +0700

    added Hello

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Helloworld {
+    public static void main(String[] args) {
+        for(int i =1; i <=10; i++)
+        {
+        System.out.println("Hello world" +i);
+    }} 
+}
